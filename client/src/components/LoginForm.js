041e0bdf2d3b99b8be8699560c29a21ca67f06db
Mario Patiño <mariopatino@users.noreply.github.com>
2022-05-11 09:09:05 -0500

    Fix on LoginForm.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,7 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
@@ -31,11 +32,12 @@ const LoginForm = () => {
         variables: { ...userFormData }
       });
 
-      if (!data) {
-        throw new Error('something went wrong!');
+      const loginData = data?.login || {}
+      if (!loginData) {
+        throw new Error('something went wrong');
       }
       
-      Auth.login(data.login.token);
+      Auth.login(loginData.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
