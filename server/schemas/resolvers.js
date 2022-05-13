060171f7d3eef350e10aeeab2c55f28837447a4d
Mario Patiño <mariopatino@users.noreply.github.com>
2022-05-12 23:25:00 -0500

    last fixed on delete books

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,61 +1,62 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
+
 const { signToken } = require('../utils/auth');
+const { User } = require('../models');
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-            if (context.user) {
-                return User.findOne({ _id: context.user._id }).populate('savedBooks');
-            } 
-            throw new AuthenticationError('You must be logged in!');
-        },
+  Query: {
+    me: async (parent, args, context) => {
+        if (context.user) {
+            return User.findOne({ _id: context.user._id }).
+                populate('savedBooks');
+        } 
+        throw new AuthenticationError('You need to be login first!');
+    },
     },
 
-    Mutation: {
-        loginUser: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
+  Mutation: {
+    loginUser: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
 
-            if (!user) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
+        if (!user) {
+            throw new AuthenticationError('Something is wrong!');
+        }
+        const isValidPasswd = await user.isCorrectPassword(password);
+        if (!isValidPasswd) {
+            throw new AuthenticationError('Something is wrong!');
+        }
+        const token = signToken(user);
 
-            const correctPw = await user.isCorrectPassword(password);
-
-            if (!correctPw) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
-
-            const token = signToken(user);
-
-            return { token, user };
-        },
-        addUser: async (parent, { username, email, password }) => {
-            const user = await User.create({ username, email, password });
-            const token = signToken(user);
-            return { token, user };
-        },
-        saveBook: async (parent, { input }, context) => {
-            if (context.user) {
-                return User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    { $addToSet: { savedBooks: input } },
-                    { new: true, runValidators: true }
-                );
-            }
-            throw new AuthenticationError('Please login to proceed');
-        },
-        removeBook: async (parent, { bookId }, context) => {
-            if (context.user) {
-                return User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    { $pull: { savedBooks: { bookId: bookId } } },
-                    { new: true }
-                );
-            }
-            throw new AuthenticationError('Please login to proceed');
-        },
+        return { token, user };
     },
+    addUser: async (parent, { username, email, password }) => {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+
+        return { token, user };
+    },
+    saveBook: async (parent, { input }, context) => {
+        if (context.user) {
+            return User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: input } },
+                { new: true, runValidators: true }
+            );
+        }
+        throw new AuthenticationError('You need to have sesion. Please login');
+    },
+    removeBook: async (parent, { bookId }, context) => {
+        if (context.user) {
+            const userDeleteBook =  User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: bookId } } },
+                { new: true }
+            );
+            return userDeleteBook;
+        }
+        throw new AuthenticationError('You need to have sesion. Please login');
+    },
+},
 };
 
 module.exports = resolvers;
